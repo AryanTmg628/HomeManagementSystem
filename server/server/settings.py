9c82ce1a8878694d2ffb13fd99164b06066aa68c
Aryan Tamang <aryantmg628@gmail.com>
2024-05-11 21:06:42 +0545

    Feat : Restframewok setting for testing so format will always be in json

--- server/server/settings.py
+++ server/server/settings.py
@@ -133,3 +133,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# restframework
+REST_FRAMEWORK = {"TEST_REQUEST_DEFAULT_FORMAT": "json"}
