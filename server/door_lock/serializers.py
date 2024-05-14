a55f01fcd5d45bd6d942970b8135a7bc267e277e
Aryan Tamang <aryantmg628@gmail.com>
2024-05-14 16:53:36 +0545

    Refactor : changed to_respresentation function

--- server/door_lock/serializers.py
+++ server/door_lock/serializers.py
@@ -11,11 +11,3 @@ class DoorLockSerializer(serializers.ModelSerializer):
     class Meta:
         model = DoorLock
         fields = "__all__"
-
-    def to_representation(self, instance):
-        """
-        run whenever we access serailizer.data and if the action is view it will remove the password
-        """
-        if "action" in self.context and self.context["action"] == "view":
-            self.fields.pop("password", None)
-        return super().to_representation(instance)
