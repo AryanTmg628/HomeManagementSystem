c3e1881f51c137c883675b466a00abe608784440
AyushTmg <ayushtmg20040722@gmail.com>
2024-05-27 22:06:02 +0545

    Feat: Added the API Endpoints for Users Skills ,Projects , and Education

---
+++ server/portfolio/serializers.py
@@ -0,0 +1,132 @@
+from .models import (
+    Portfolio,
+    Education,
+    Skill,
+    Project,
+    Contact
+)
+
+
+from rest_framework import serializers
+
+
+
+
+
+
+
+
+
+# ! Serializer For Users Education 
+class EducationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Education
+        fields=[
+            'id',
+            'institution_name',
+            'degree',
+            'date_from',
+            'date_to',
+        ]
+
+    
+    def create(self, validated_data):
+        """
+        Overriding the create method
+        """
+        portfolio_id=self.context['portfolio_id']
+
+        return Education.objects.create(
+            portfolio_id=portfolio_id,
+            **validated_data
+        )
+
+
+
+
+# ! Serializer For Users Skills 
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Skill
+        fields=[
+            'id',
+            'icon',
+            'name',
+            'level'
+        ]
+
+
+    
+    def create(self, validated_data):
+        """
+        Overriding the create method
+        """
+        portfolio_id=self.context['portfolio_id']
+
+        return Skill.objects.create(
+            portfolio_id=portfolio_id,
+            **validated_data
+        )
+
+
+
+
+# ! Serializer For Project Serializer 
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Project
+        fields=[
+            'id',
+            'title',
+            'description',
+            'live_link',
+            'github_link',
+        ]
+
+
+
+    def create(self, validated_data):
+        """
+        Overriding the create method
+        """
+        portfolio_id=self.context['portfolio_id']
+
+        return Project.objects.create(
+            portfolio_id=portfolio_id,
+            **validated_data
+        )
+        
+
+
+
+# ! Serializer For Contacting User 
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Contact
+        fields=[
+            'id',
+            'fullname',
+            'email',
+            'subject',
+            'message',
+            'date'
+        ] 
+
+
+
+
+
+# ! Serializer For Users Portfolio 
+class PortfolioSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Portfolio
+        fields = [
+            'pk',
+            'image',
+            'description',
+            'location',
+            'instagram_link',
+            'github_link',
+            'contact_no'
+        ]
--- server/portfolio/views.py
+++ server/portfolio/views.py
@@ -1,3 +1,129 @@
-from django.shortcuts import render
+from utils.response.response import CustomResponse as cr
+from utils.exception.exception import CustomException as ce
+from .serializers import (
+    PortfolioSerializer,
+    EducationSerializer,
+    SkillSerializer,
+    ProjectSerializer
+)
+from .models import (
+    Portfolio,
+    Education,
+    Skill,
+    Project,
+)
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+
+
+
+# ! ViewSet For Portfolio Model 
+class PortfolioViewSet(ModelViewSet):
+    queryset=Portfolio.objects.all()
+    http_method_names=['retrieve','patch']
+    serializer_class=PortfolioSerializer  
+
+
+
+
+# ! ViewSet For Users Education 
+class EducationViewSet(ModelViewSet):
+    serializer_class=EducationSerializer
+
+
+    def get_queryset(self):
+        """
+        Overriding the base quesryset to filter the 
+        users education by portfolio pk 
+        """
+        portfolio_pk=self.kwargs['portfolio_pk']
+
+        return  Education.objects.filter(
+            portfolio_id=portfolio_pk
+        )
+    
+
+    def get_serializer_context(self):
+        """
+        Passing context to serailizer
+        """
+        portfolio_pk=self.kwargs['portfolio_pk']
+        return {
+            'portfolio_id':portfolio_pk
+        }
+    
+
+
+
+# ! ViewSet For Users Skill 
+class SkillViewSet(ModelViewSet):
+    serializer_class=SkillSerializer
+
+
+    def get_queryset(self):
+        """
+        Overriding the base quesryset to filter the 
+        users skills by portfolio pk 
+        """
+        portfolio_id=self.kwargs['portfolio_pk']
+        
+        return Skill.objects.filter(
+            portfolio_id=portfolio_id
+        )
+    
+
+    def get_serializer_context(self):
+        """
+        Passing context to serailizer
+        """
+        portfolio_pk=self.kwargs['portfolio_pk']
+        return {
+            'portfolio_id':portfolio_pk
+        }
+
+
+
+
+# ! ViewSet For Users Project 
+class ProjectViewSet(ModelViewSet):
+    serializer_class=ProjectSerializer
+
+    def get_queryset(self):
+        """
+        Overriding the base quesryset to filter the 
+        users project by portfolio pk 
+        """
+        portfolio_id=self.kwargs['portfolio_pk']
+        
+        return Project.objects.filter(
+            portfolio_id=portfolio_id
+        )
+    
+
+    def get_serializer_context(self):
+        """
+        Passing context to serailizer
+        """
+        portfolio_pk=self.kwargs['portfolio_pk']
+        
+        return {
+            'portfolio_id':portfolio_pk
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
